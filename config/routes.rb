5007a61c2568e4812953ed6975f30bba227d600f
Greg Robinson <gjrobinson@ymail.com>
2016-11-09 23:55:10 -0700

    Add user profiles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/:username', to: 'users#show', as: 'user'
+
   resources :items
   resources :credentialing_documents
   resources :tweets
   ActiveAdmin.routes(self)
   devise_for :users
   as :user do
-    get "signin" => 'devise/sessions#new'
-    delete "signout" => 'devise/sessions#destroy'
-    get "signup" => 'devise/registrations#new'
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get "signup", to: 'devise/registrations#new'
   end
   root 'pages#home'
-  get  'about' => 'pages#about'
-  get  'contact' => 'pages#contact'
+  get  'about', to: 'pages#about'
+  get  'contact', to: 'pages#contact'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
